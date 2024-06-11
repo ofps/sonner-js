1ed2e5e2c0c126c46168334be3ad2f0114fb441c
Lachee <lachlan.h@hotmail.com>
2024-06-11 16:46:04 +1000

    Replaced uglifer with a proper minifying setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+    plugins: [new MiniCssExtractPlugin({
+      filename: "sonner.min.css",
+    })],
+    entry: [
+        './src/sonner.js',
+        './src/sonner.css',
+    ],
+    output: {
+        filename: 'sonner.min.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+          // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+          `...`,
+          new CssMinimizerPlugin(),
+        ],
+      },
+};
